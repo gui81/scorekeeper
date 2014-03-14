7ed9146a0dcdea4d172821dfc63cc8d163a55da3
Lucas Johnson <lucasejohnson@netscape.net>
2014-03-14 01:10:46 -0400

    Made serveral updates, including:
    Added a new player form
    Added a Rules page
    Beginning to calculate Elo Rating for players
    Created a Players collection to store names separately

--- collections/models.js
+++ collections/models.js
@@ -1,29 +1,31 @@
 // using Collections2 pkg to validate inserts against schema to ensure data
 // integrity
 Matches = new Meteor.Collection('matches');
+Players = new Meteor.Collection('players');
+EloRatings = new Meteor.Collection('elo_ratings');
 
 // here we are creating a SimpleSchema because date_time is in the Collection,
 // but not in the form
 MatchFormSchema = new SimpleSchema({
   ro: {
     type: String,
-    label: "Red Offensive Player*",
+    label: "Red Offense*",
     min: 2
   },
   rd: {
     type: String,
-    label: "Red Defensive Player",
+    label: "Red Defense",
     optional: true,
     min: 2
   },
   bo: {
     type: String,
-    label: "Blue Offensive Player*",
+    label: "Blue Offense*",
     min: 2
   },
   bd: {
     type: String,
-    label: "Blue Defensive Player",
+    label: "Blue Defense",
     optional: true,
     min: 2
   },
@@ -41,19 +43,177 @@ MatchFormSchema = new SimpleSchema({
   }
 });
 
+PlayerFormSchema = new SimpleSchema({
+  player_name: {
+    type: String,
+    label: "Name*",
+    min: 2
+  },
+  rating: {
+    type: Number,
+    label: "Initial Rating*",
+    allowedValues: [250, 750, 1250, 1750, 2250]
+  }
+});
+
 if (Meteor.isServer) {
+  Meteor.publish('matches', function() {
+    return Matches.find();
+  })
+
+  Meteor.publish('players', function() {
+    return Players.find();
+  })
+
+  Meteor.publish('elo_ratings', function() {
+    return EloRatings.find();
+  })
+
+  // define some constants for Elo Ratings
+  // we are using the Bonzini USA values:
+  //   http://www.bonziniusa.com/foosball/tournament/TournamentRankingSystem.html
+  var K_RATING_COEFFICIENT = 50;
+  var F_RATING_INTERVAL_SCALE_WEIGHT = 1000;
+
+  var updateRating = function(player_id, rating, opponent_rating, win) {
+    var S = (win ? 1 : 0);
+    var We = winExpectancy(rating, opponent_rating);
+    var Rn = Ro + (K_RATING_COEFFICIENT * (S - We));
+
+    EloRatings.insert({
+      date_time: Date.now(),
+      player_id: player_id,
+      rating: Rn
+    });
+
+    return Rn;
+  }
+
+  var winExpectancy = function(rating, opponent_rating) {
+    var We = 1 / (
+        Math.pow(10, (-(rating-opponent_rating)/F_RATING_INTERVAL_SCALE_WEIGHT))
+        + 1);
+    return We;
+  }
+
+  var addPlayer = function(player_name, rating) {
+    var id = Players.findOne({name: player_name});
+    if (id) {
+      // player already in database, no need to add again
+      return id;
+    }
+
+    // didn't find player above, so add one now
+    id = Players.insert({
+      name: player_name
+    });
+
+    // add a rating
+    EloRatings.insert({
+      date_time: Date.now(),
+      player_id: id,
+      rating: rating
+    });
+
+    return id;
+  }
+
+  var getPlayerId = function(player_name) {
+    var id = Players.findOne({name: player_name});
+    if (!id) {
+      // if an id did not exist, then it means that they were not added through
+      // the add player form, no biggie, we'll just add them as a Novice-Elite (750)
+      var id = addPlayer(player_name, 750);
+    }
+
+    return id;
+  }
+
   Meteor.startup(function () {
     // code to run on server at startup
     Meteor.methods({
       add_match: function(doc) {
         check(doc, MatchFormSchema);
-        doc_with_date_time = doc;
-        doc_with_date_time['date_time'] = Date.now;
+        //doc['date_time'] = Date.now();
         // console.log('ro = ' + doc.ro + ", rd = " + doc.rd +
         //             ', bo = ' + doc.bo + ", bd = " + doc.bd +
-        //             ', rs = ' + doc.rs + ", bs = " + doc.bs);
+        //             ', rs = ' + doc.rs + ", bs = " + doc.bs +
+        //             ', date_time = ' + doc.date_time);
+        //Matches.insert(doc);
 
-        Matches.insert(doc_with_date_time);
+        var ro_id, rd_id, bo_id, bd_id;
+        var last_ro_rating, last_rd_rating, last_bo_rating, last_bd_rating;
+        if (typeof doc.ro != "undefined") {
+          ro_id = getPlayerId(doc.ro);
+          last_ro_rating = EloRatings.find({player_id: ro_id}, {sort: {date_time: -1}, limit: 1});
+        }
+        if (typeof doc.rd != "undefined") {
+          rd_id = getPlayerId(doc.rd);
+          last_rd_rating = EloRatings.find({player_id: rd_id}, {sort: {date_time: -1}, limit: 1});
+        }
+        if (typeof doc.bo != "undefined") {
+          bo_id = getPlayerId(doc.bo);
+          last_bo_rating = EloRatings.find({player_id: bo_id}, {sort: {date_time: -1}, limit: 1});
+        }
+        if (typeof doc.bd != "undefined") {
+          bd_id = getPlayerId(doc.bd);
+          last_bd_rating = EloRatings.find({player_id: bd_id}, {sort: {date_time: -1}, limit: 1});
+        }
+
+        Matches.insert({
+          date_time: Date.now(),
+          ro_id: ro_id,
+          rd_id: rd_id,
+          bo_id: bo_id,
+          bd_id: bd_id,
+          rs: doc.rs,
+          bs: doc.bs
+        });
+
+        var red_won;
+        if (parseInt(doc.rs) > parseInt(doc.bs)) {
+          red_won = true;
+        } else {
+          red_won = false;
+        }
+
+        if ((typeof doc.rd != "undefined") &&
+            (typeof doc.bd != "undefined")) {
+          // 2 v 2
+          var red_rating = (last_ro_rating + last_rd_rating) / 2.0;
+          var blue_rating = (last_bo_rating + last_bd_rating) / 2.0;
+          var ro_rn = updateRating(ro_id, red_rating, blue_rating, red_won);
+          var rd_rn = updateRating(rd_id, red_rating, blue_rating, red_won);
+          var bo_rn = updateRating(bo_id, blue_rating, red_rating, !red_won);
+          var bd_rn = updateRating(bd_id, blue_rating, red_rating, !red_won);
+        } else if ((typeof doc.rd == "undefined") &&
+                   (typeof doc.bd == "undefined")) {
+          // 1 v 1
+          var ro_rn = updateRating(ro_id, last_ro_rating, last_bo_rating, red_won);
+          var bo_rn = updateRating(bo_id, last_bo_rating, last_ro_rating, !red_won);
+        } else {
+          // this is where I give more value to the single player, by not dividing
+          // the team rating by 2, but instead by 1.5, since they are expected to
+          // win
+          if (typeof doc.rd != "undefined") {
+            // 2 red v 1 blue
+            var red_rating = (last_ro_rating + last_rd_rating) / 1.5;
+            var ro_rn = updateRating(ro_id, red_rating, last_bo_rating, red_won);
+            var rd_rn = updateRating(rd_id, red_rating, last_bo_rating, red_won);
+            var bo_rn = updateRating(bo_id, last_bo_rating, red_rating, !red_won);
+          } else {
+            // 1 red v 2 blue
+            var blue_rating = (last_bo_rating + last_bd_rating) / 1.5;
+            var ro_rn = updateRating(ro_id, last_ro_rating, blue_rating, red_won);
+            var bo_rn = updateRating(bo_id, blue_rating, last_ro_rating, !red_won);
+            var bd_rn = updateRating(bd_id, blue_rating, last_ro_rating, !red_won);
+          }
+        }
+      },
+
+      add_player: function(doc) {
+        check(doc, PlayerFormSchema);
+        addPlayer(doc.player_name, doc.rating);
       }
     });
   });
--- scorekeeper.js
+++ scorekeeper.js
@@ -1,5 +1,36 @@
 if (Meteor.isClient) {
   SimpleSchema.debug = true;
+  Meteor.Spinner.options = {
+    lines: 13, // The number of lines to draw
+    length: 5, // The length of each line
+    width: 2, // The line thickness
+    radius: 8, // The radius of the inner circle
+    corners: 0.7, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#fff', // #rgb or #rrggbb
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: true, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: '20px', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
+
+  Handlebars.registerHelper("formatDate", function(datetime) {
+    var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                       "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+    var dt = new Date(datetime);
+    var d = dt.getDate();
+    var m = monthNames[dt.getMonth()];
+    var y = dt.getFullYear();
+    var h = dt.getHours();
+    var min = dt.getMinutes();
+    var s = dt.getSeconds();
+    return (y + " " + m + " " + d + " " + pad(h,2) + ":" + pad(min,2) + ":" + pad(s,2));
+  });
 
   var mForm = new AutoForm(MatchFormSchema);
   Template.game_form.helpers({
@@ -8,7 +39,150 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.individual_scores.helpers({
+  var pForm = new AutoForm(PlayerFormSchema);
+  Template.player_form.helpers({
+    playerForm: function() {
+      return pForm;
+    }
+  });
+
+  Template.header.events({
+    "click #menu-toggle": function(evt, tmpl) {
+      $("#wrapper").toggleClass("active");
+    }
+  });
+
+  Template.individual_stats.rendered = function() {
+    $("table#individual_stats_table").tablesorter({sortList:[[3,1]]});
+  }
+
+  Template.team_stats.rendered = function() {
+    $("table#team_stats_table").tablesorter({sortList:[[4,1]]});
+  }
+
+  Template.last_10_matches.rendered = function() {
+    $("table#last_10_matches_table").tablesorter({sortList:[[0,1]]});
+  }
+
+  Template.last_10_players.rendered = function() {
+    $("table#last_10_players_table").tablesorter({sortList:[[0,1]]});
+  }
+
+  var pad = function(num, size) {
+    var s = num + "";
+    while (s.length < size) {
+      s = "0" + s;
+    }
+    return s;
+  }
+
+  var addIndPlayersArray = function(players, name, win, loss) {
+    if (typeof players[name] != "undefined") {
+      players[name] = ({
+        wins: players[name].wins + win,
+        losses: players[name].losses + loss
+      });
+    } else {
+      if (typeof name != "undefined") {
+        players[name] = ({
+          wins: win,
+          losses: loss
+        });
+      }
+    }
+  }
+
+  var addTeamPlayersArray = function(players, o_name, d_name, win, loss) {
+    if ((typeof players[o_name] != "undefined") &&
+        (typeof players[o_name][d_name] != "undefined")) {
+      players[o_name][d_name] = ({
+        wins: players[o_name][d_name].wins + win,
+        losses: players[o_name][d_name].losses + loss
+      });
+    } else {
+      if ( (typeof o_name != "undefined") && (typeof d_name != "undefined") ) {
+        if (typeof players[o_name] == "undefined") {
+          players[o_name] = [];
+        }
+        players[o_name][d_name] = ({
+          wins: win,
+          losses: loss
+        });
+      }
+    }
+  }
+
+  Template.game_form.helpers({
+    red_singles_wins: function() {
+      var matches = Matches.find({});
+      var wins = 0;
+      matches.forEach(function(match) {
+        if ((typeof match.ro != "undefined") &&
+            (typeof match.rd == "undefined") &&
+            (parseInt(match.rs) > parseInt(match.bs))) {
+          wins += 1;
+        }
+      });
+
+      return wins;
+    },
+
+    red_doubles_wins: function() {
+      var matches = Matches.find({});
+      var wins = 0;
+      matches.forEach(function(match) {
+        if ((typeof match.ro != "undefined") &&
+            (typeof match.rd != "undefined") &&
+            (parseInt(match.rs) > parseInt(match.bs))) {
+          wins += 1;
+        }
+      });
+
+      return wins;
+    },
+
+    blue_singles_wins: function() {
+      var matches = Matches.find({});
+      var wins = 0;
+      matches.forEach(function(match) {
+        if ((typeof match.bo != "undefined") &&
+            (typeof match.bd == "undefined") &&
+            (parseInt(match.bs) > parseInt(match.rs))) {
+          wins += 1;
+        }
+      });
+
+      return wins;
+    },
+
+    blue_doubles_wins: function() {
+      var matches = Matches.find({});
+      var wins = 0;
+      matches.forEach(function(match) {
+        if ((typeof match.bo != "undefined") &&
+            (typeof match.bd != "undefined") &&
+            (parseInt(match.bs) > parseInt(match.rs))) {
+          wins += 1;
+        }
+      });
+
+      return wins;
+    }
+  });
+
+  Template.last_10_matches.helpers({
+    matches: function() {
+      return Matches.find({}, {sort: {date_time: -1}, limit: 10});
+    }
+  });
+
+  Template.last_10_players.helpers({
+    players: function() {
+      return Players.find({}, {limit: 10});
+    }
+  });
+
+  Template.individual_stats.helpers({
     score: function() {
       var cursor = Matches.find({});
       var players = {};
@@ -22,70 +196,22 @@ if (Meteor.isClient) {
           blue_win = 1;
         }
 
-        if (typeof players[match.ro] != "undefined") {
-          players[match.ro] = ({
-            wins: players[match.ro].wins + red_win,
-            losses: players[match.ro].losses + blue_win
-          });
-        } else {
-          if (typeof match.ro != "undefined") {
-            players[match.ro] = ({
-              wins: red_win,
-              losses: blue_win
-            });
-          }
-        }
-
-        if (typeof players[match.rd] != "undefined") {
-          players[match.rd] = ({
-            wins: players[match.rd].wins + red_win,
-            losses: players[match.rd].losses + blue_win
-          });
-        } else {
-          if (typeof match.rd != "undefined") {
-            players[match.rd] = ({
-              wins: red_win,
-              losses: blue_win
-            });
-          }
-        }
-
-        if (typeof players[match.bo] != "undefined") {
-          players[match.bo] = ({
-            wins: players[match.bo].wins + blue_win,
-            losses: players[match.bo].losses + red_win
-          });
-        } else {
-          if (typeof match.bo != "undefined") {
-            players[match.bo] = ({
-              wins: blue_win,
-              losses: red_win
-            });
-          }
-        }
-
-        if (typeof players[match.bd] != "undefined") {
-          players[match.bd] = ({
-            wins: players[match.bd].wins + blue_win,
-            losses: players[match.bd].losses + red_win
-          })
-        } else {
-          if (typeof match.bd != "undefined") {
-            players[match.bd] = ({
-              wins: blue_win,
-              losses: red_win
-            });
-          }
-        }
+        addIndPlayersArray(players, match.ro, red_win, blue_win);
+        addIndPlayersArray(players, match.rd, red_win, blue_win);
+        addIndPlayersArray(players, match.bo, blue_win, red_win);
+        addIndPlayersArray(players, match.bd, blue_win, red_win);
       });
 
       // now create a list that can be used for display
       var p = [];
       for (var player in players) {
+        var per = (players[player].wins /
+          (players[player].wins + players[player].losses));
         p.push({
           name: player,
           wins: players[player].wins,
-          losses: players[player].losses
+          losses: players[player].losses,
+          percent: (per.toFixed(2)*100) + "%"
         });
       }
 
@@ -93,11 +219,12 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.team_scores.helpers({
+  Template.team_stats.helpers({
     score: function() {
       var cursor = Matches.find({});
-      var teams = [];
-      // loop through Matches to find all individuals and tally up scores
+      var players = {};
+
+      // loop through Matches to find all teams and tally up scores
       cursor.forEach(function(match) {
         var red_win = 0;
         var blue_win = 0;
@@ -107,19 +234,24 @@ if (Meteor.isClient) {
           blue_win = 1;
         }
 
-        teams.push({
-          off_player: match.ro,
-          def_player: match.rd,
-          wins: red_win,
-          losses: blue_win
-        });
-        teams.push({
-          off_player: match.bo,
-          def_player: match.bd,
-          wins: blue_win,
-          losses: red_win
-        });
+        addTeamPlayersArray(players, match.ro, match.rd, red_win, blue_win);
+        addTeamPlayersArray(players, match.bo, match.bd, blue_win, red_win);
       });
+
+      var teams = [];
+      for (var o_name in players) {
+        for (var d_name in players[o_name]) {
+          var per = (players[o_name][d_name].wins /
+            (players[o_name][d_name].wins + players[o_name][d_name].losses));
+          teams.push({
+            off_player: o_name,
+            def_player: d_name,
+            wins: players[o_name][d_name].wins,
+            losses: players[o_name][d_name].losses,
+            percent: (per.toFixed(2)*100) + "%"
+          });
+        }
+      }
 
       return teams;
     }
