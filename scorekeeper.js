7c7d74926e3b68cd561bcb439d3463fac7377333
Lucas Johnson <lucasejohnson@netscape.net>
2014-03-26 09:47:17 -0400

    Using jqueryui autocomplete instead of typeahead.js.
    typeahead.js brought in a lot of unwanted css changes, which is strange since
    you would think it would have the same look and feel as bootstrap, but it
    doesnt.  Now using jqueryui autocomplete which is actually easier to use as
    well (we dont need anything fancy).

--- scorekeeper.js
+++ scorekeeper.js
@@ -140,30 +140,6 @@ if (Meteor.isClient) {
     $("table#last_10_players_table").tablesorter({sortList:[[0,0]]});
   }
 
-  var substringMatcher = function(strs) {
-    return function findMatches(q, cb) {
-      var matches, substringRegex;
-
-      // an array that will be populated with substring matches
-      matches = [];
-
-      // regex used to determine if a string contains the substring `q`
-      substrRegex = new RegExp(q, 'i');
-
-      // iterate through the pool of strings and for any string that
-      // contains the substring `q`, add it to the `matches` array
-      $.each(strs, function(i, str) {
-        if (substrRegex.test(str)) {
-          // the typeahead jQuery plugin expects suggestions to a
-          // JavaScript object, refer to typeahead docs for more info
-          matches.push({ value: str });
-        }
-      });
-
-      cb(matches);
-    };
-  };
-
   Template.game_form.rendered = function() {
     var players = Players.find({}).fetch();
     var names = [];
@@ -171,15 +147,8 @@ if (Meteor.isClient) {
       names.push(player.name);
     });
 
-    $('.typeahead').typeahead({
-      hint: true,
-      highlight: true,
-      minLength: 1
-    },
-    {
-      name: 'names',
-      displayKey: 'value',
-      source: substringMatcher(names)
+    $(".input_autocomplete").autocomplete({
+      source: names
     });
   }
 
